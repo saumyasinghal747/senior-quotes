5a204e8529e734685ae766491737035f9d998a81
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-05-22 12:26:20 -0700

    style: shuffle responses

--- pages/index.vue
+++ pages/index.vue
@@ -14,16 +14,35 @@
 </template>
 
 <script>
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 export default {
   async asyncData({$content}){
     const spsht = await $content('responses').fetch();
-    spsht.body = spsht.body.map(e=>{
+    spsht.body = shuffle(spsht.body.map(e=>{
       e.response = e['Describe a positive memory involving one or more members of the class of 2021'][' What impact did this have on you?']
       e.timestamp = e['Timestamp']
       e.grade = e['What grade are you in?']
       e.name = e['Name (optional)']
       return e
-    })
+    }))
+
     return {spsht}
   },
 methods:{
