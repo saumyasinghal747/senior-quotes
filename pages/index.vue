862da13507a73fc0f0c35bafb21584f0cea5f7f0
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-05-22 10:41:35 -0700

    style: align card footer

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
     <div
       class="px-10 py-6 col-span-1 bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg transition-shadow duration-200 mx-10 md:mx-0 hover:shadow-2xl rounded-xl shadow-md"
      :key="note.timestamp" v-for="note in spsht.body">
-      <p  class="text-white">{{note.response}}</p>
-      <div class="text-right font-semibold text-sm pt-4 ">
+      <p  class="text-white pb-6">{{note.response}}</p>
+      <div class="text-right fixed bottom-6 right-10 font-semibold text-sm pt-4 ">
         <span class="text-gray-100 opacity-70">{{note.name || 'Anonymous'}} •  {{note.grade}}</span>
 
         <span class="text-red-700">• {{ niceDate(note.timestamp) }}</span>
