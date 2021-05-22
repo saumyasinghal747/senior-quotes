f95be1a3e70137793909a5b0a6705454f5ac8576
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-05-21 23:47:58 -0700

    style: backdrop blurs

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,31 @@
 <template>
-  <div class=" ">
-  <h1 class="text-4xl pt-20 mx-10 md:mx-20 font-bold text-red-700">Class of 2021 Send-Off</h1>
+  <div class="min-h-screen">
+  <h1 class="text-4xl pt-20 mx-10 md:mx-20 font-bold text-white">Class of 2021 Send-Off</h1>
     <Nuxt />
-    <div class=" px-10 md:px-20 py-5 fixed w-full bottom-0 h-16 bg-gray-200">
+    <div class=" px-10 md:px-20 py-5 bg-white bg-opacity-20 text-white  fixed w-full bottom-0 h-16 backdrop-filter backdrop-blur-xl">
       <nuxt-content v-if="footer" :document="footer"/>
     </div>
   </div>
 </template>
 
 <style>
+body {
+    background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.0), rgba(255,255,255, 0.0)), 
+    url("https://wallpaperaccess.com/full/2897934.jpg");
+    min-height:100vh;
 
+    background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
+}
 </style>
 <script>
+// https://i.pinimg.com/originals/75/a3/e8/75a3e8d47a81009cceef996c0410f6b1.jpg
+//https://wallpaperaccess.com/full/2897934.jpg
+//https://wallpaperaccess.com/full/1442113.jpg
+
+// https://wallpaperaccess.com/full/1320116.jpg
 export default {
 
   data:()=>({footer:null}),
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="grid gap-10 lg:grid-cols-3 md:grid-cols-2 my-20 mx-20">
     <div
-      class="px-10 py-6 col-span-1 bg-white transition-shadow duration-200 mx-10 md:mx-0 hover:shadow-2xl rounded-xl shadow-lg"
+      class="px-10 py-6 col-span-1 bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg transition-shadow duration-200 mx-10 md:mx-0 hover:shadow-2xl rounded-xl shadow-md"
      :key="note.timestamp" v-for="note in spsht.body">
-      <p>{{note.response}}</p>
-      <div class="text-right text-sm pt-4 ">
-        <span class="text-gray-500">{{note.name || 'Anonymous'}} - {{note.grade}}</span>
+      <p  class="text-white">{{note.response}}</p>
+      <div class="text-right font-semibold text-sm pt-4 ">
+        <span class="text-gray-100 opacity-70">{{note.name || 'Anonymous'}} •  {{note.grade}}</span>
 
         <span class="text-red-700">• {{ niceDate(note.timestamp) }}</span>
         </div>
@@ -16,7 +16,14 @@
 <script>
 export default {
   async asyncData({$content}){
-    const spsht = await $content('testsheet1').fetch();
+    const spsht = await $content('responses').fetch();
+    spsht.body = spsht.body.map(e=>{
+      e.response = e['Describe a positive memory involving one or more members of the class of 2021'][' What impact did this have on you?']
+      e.timestamp = e['Timestamp']
+      e.grade = e['What grade are you in?']
+      e.name = e['Name (optional)']
+      return e
+    })
     return {spsht}
   },
 methods:{
