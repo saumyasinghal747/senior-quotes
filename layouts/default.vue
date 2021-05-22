9433ba12199495abd23d8516a56387d27ece082b
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-05-21 19:03:02 -0700

    feat:  parse csv file to cards

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
+  <div class="min-h-screen h-full py-20 bg-gray-100">
+    <div v-if="false" class=" text-center   bg-green-600 text-white">
+      <h1 class="lg:text-7xl md:text-6xl sm:text-5xl text-4xl my-auto py-20 sm:py-28 md:py-40 lg:py-44">Class of 2021 Send-off</h1>
+      </div>
+  <h1 class="text-4xl mx-10 md:mx-20 font-bold text-green-500">Class of 2021 Send-Off</h1>
     <Nuxt />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="container">
-    {{spsht}}
+  <div class="grid gap-10 lg:grid-cols-3 md:grid-cols-2 my-20 mx-20">
+    <div
+      class="px-10 py-6 col-span-1 bg-white mx-10 md:mx-0 rounded-md shadow-lg"
+     :key="note.timestamp" v-for="note in spsht.body">
+      <p>{{note.response}}</p>
+      <div class="text-right pt-4 text-gray-400">{{note.name || 'Anonymous'}} - {{note.grade}}</div>
+    </div>
   </div>
 </template>
 
