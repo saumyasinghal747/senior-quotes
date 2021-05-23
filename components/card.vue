77bc13ca2c4368c798d7aa8021a1507350b296a8
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-05-22 17:36:20 -0700

    style: preserve line breaks

--- components/card.vue
+++ components/card.vue
@@ -2,7 +2,7 @@
   
 <div 
       class="px-10 pb-12 my-3 transform translate-x-0 py-6 col-span-1 bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg transition-shadow duration-200 mx-0 hover:shadow-2xl rounded-xl shadow-md">
-      <p  class="text-white  pb-10">{{note.response}}</p>
+      <p  class="text-white whitespace-pre-wrap pb-10">{{note.response}}</p>
       <div class="text-right fixed bottom-6  right-10 font-semibold text-xs  ">
         <span class="text-gray-100 opacity-70">{{note.name || 'Anonymous'}} •  {{note.grade}}</span>
 
