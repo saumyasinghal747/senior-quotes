4b9aa3da5800d6ddd2c3f6427660ed15c26cada2
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-06-16 13:53:30 +0000

    deploy: 77bc13ca2c4368c798d7aa8021a1507350b296a8

--- _nuxt/static/1623850789/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsenior-notes\u002F_nuxt\u002Fstatic\u002F1623850789",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fsenior-notes\u002F",assetsPath:"\u002Fsenior-notes\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9818d8d9"}}}}(null));
