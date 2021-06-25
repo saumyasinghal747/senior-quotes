c0d919387e7278df1537c37d47035158ad8b0f60
saumyasinghal747 <saumyasinghal747@users.noreply.github.com>
2021-06-25 20:56:47 +0000

    deploy: 77bc13ca2c4368c798d7aa8021a1507350b296a8

---
+++ _nuxt/static/1624654606/embed/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsenior-notes\u002F_nuxt\u002Fstatic\u002F1624654606",layout:"embed",error:a,serverRendered:true,routePath:"\u002Fembed",config:{_app:{basePath:"\u002Fsenior-notes\u002F",assetsPath:"\u002Fsenior-notes\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"127ef0ec"}}}}(null));
